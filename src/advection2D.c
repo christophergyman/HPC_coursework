66e6a78506a9e994c9130289c6fafb796aae53d9
christophergyman <christophergayiuman@gmail.com>
2024-02-26 12:40:32 +0000

    fix bug for collapse(2) push

--- src/advection2D.c
+++ src/advection2D.c
@@ -185,7 +185,7 @@ int main(){
     /*** Calculate rate of change of u using leftward difference ***/
     /* Loop over points in the domain but not boundary values */
     /* LOOP 8 */
-	#pragma omp parallel collapse(2) for default(none) shared(highest_velx, NX, NY, dudt, y,  vely, u, dx, dy, friction_vel, y0) private(velx)
+	#pragma omp parallel for collapse(2) default(none) shared(highest_velx, NX, NY, dudt, y,  vely, u, dx, dy, friction_vel, y0) private(velx)
     for (int i=1; i<NX+1; i++){
       for (int j=1; j<NY+1; j++){
 			velx = horizontal_wind_velocity(y[j], 1.0, friction_vel);
@@ -209,7 +209,7 @@ int main(){
     /*** Update u from t to t+dt ***/
     /* Loop over points in the domain but not boundary values */
     /* LOOP 9 */
-	#pragma omp parallel collapse(2) for default(shared)
+	#pragma omp parallel for collapse(2) default(shared)
 	for	(int i=1; i<NX+1; i++){
       for (int j=1; j<NY+1; j++){
 	    u[i][j] = u[i][j] + dudt[i][j] * dt;
